262267d3283767e7af4da99d5c9941310d6409ee
Imron02 <imron02@gmail.com>
2025-02-26 17:06:23 +0700

    refactor: change to update manual

--- expo-updates-client/App.js
+++ expo-updates-client/App.js
@@ -1,13 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
 import Constants from 'expo-constants';
+import { StatusBar } from 'expo-status-bar';
+import * as Updates from "expo-updates";
+import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      console.error(error);
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app 3!</Text>
+      <Text>Open up App.js to start working on your app 1!</Text>
       <Text>{Constants.expoConfig.name}</Text>
       <Image source={require('./assets/favicon.png')} />
+
+      <Button title="Fetch update" onPress={onFetchUpdateAsync} />
       <StatusBar style="auto" />
     </View>
   );
